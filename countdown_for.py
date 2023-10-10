374be2b1fbd69a51e7275e7e6b350b28895e5d63
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-10-10 12:51:37 +0700

    Update countdown_for.py

--- countdown_for.py
+++ countdown_for.py
@@ -1,6 +1,8 @@
 # https://www.geeksforgeeks.org/how-to-add-time-delay-in-python/
 # https://sites.pitt.edu/~naraehan/python3/mbb15.html
-# Bạn hãy viết lại chương trình dùng for
+# Thời gian: 10 phút
+# 5 phút đầu: time.sleep(1) có tác dụng gì?
+# 5 phút cuối: hãy viết lại chương trình dùng for
 # (vì trong trường hợp này số lần lặp đã biết trước)
 import time
 countdown = 10
