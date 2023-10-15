74af34c45e52e6b2c3eec39a9b33088b2939d0c9
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-10-15 11:30:52 +0700

    Update sum_of_digits.py

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,7 +1,7 @@
 # Nhập vào số nguyên dương n.
 # Tính tổng các chữ số của n
-n=int(input("Nhập số nguyên dương n = "))
-sum_of_digits=0
+n = int(input("Nhập số nguyên dương n = "))
+sum_of_digits = 0
 while n>0:
     sum_of_digits += n%10
     n //= 10
