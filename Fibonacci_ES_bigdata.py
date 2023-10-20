467ae3c1c8f1be5d5c568aea9b5039fecd7143fd
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-10-20 12:04:24 +0700

    Update Fibonacci_ES_bigdata.py

--- Fibonacci_ES_bigdata.py
+++ Fibonacci_ES_bigdata.py
@@ -7,29 +7,30 @@ I now increase it to 50_000
 
 def fibo(n):
     # a beautiful idea: number as string
-    if n == 1: return '1'
-    elif n == 2: return '1'    # '2' for Lucas sequence
+    if n == 0: return '2'    # '2' for Lucas sequence
+    elif n == 1: return '1'    
     else:
-        a, b = '1', '1'
-        for i in range(3, n + 1):
+        a, b = '0', '1'
+        for i in range(2, n + 1):
             a, b = b, str(int(a) + int(b))
         return b
 
 # Amazing! I can get 50_000 - th Fibonacci
 n=2000
-# only n-th Fibo
-s = fibo(n)
-print('s = ',s,' with ',len(s))
-print(f'Probabilities of the digits of the {n}-th Fibonacci sequence:')
-for i in range(10):
-    print(f'  {str(i)} is {s.count(str(i)) * 100 /len(s):>5.2f} %') 
-
 # from 1 to n-th Fibo
-t = ''.join(fibo(i) for i in range(1,n+1))
+string_all_Fibos = t = ''.join(fibo(i) for i in range(1,n+1))
 print('t = ',t,' with ',len(t))
 print('Probabilities of the digits of the whole Fibonacci sequence:')
 for i in range(10):
     print(f'  {str(i)} is {t.count(str(i)) * 100 /len(t):>5.2f} %')
-print('\nConjecture: for any initial conditions (1,1) or (1,2)')
+
+print('\nConjecture: for any initial conditions, (0,1)-Fibonacci or (2,1)-Lucas,')
 print('the digits of the whole sequence x_{n+2}=x_{n+1}+x_n')
 print('follows the uniform distribution!')
+
+""" REFERENCE
+[1] 2016 https://math.stackexchange.com/questions/1845099/is-the-fibonacci-constant-0-11235813213455-a-normal-number
+[2] 2022 preprint, The Fibonacci Sequence is Normal Base 10
+    https://arxiv.org/abs/2202.08986
+
+"""
