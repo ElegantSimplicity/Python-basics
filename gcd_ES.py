6945672a66b9df37ef7555cfa4e9e0a171e877cc
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-12-26 22:51:22 +0700

    Add files via upload

---
+++ gcd_ES.py
@@ -0,0 +1,20 @@
+"""Calculate gcd and lcm of 2 natural numbers"""
+
+def gcd(a, b):
+    """Calculate gcd of a and b (the Euclidean Algorithm)"""
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+def lcm(a, b):
+    """Calculate lcm of a and b"""
+    return int((a * b) / gcd(a,b))
+
+# Driver code
+if __name__ == "__main__":
+    a = 16
+    b = 40
+
+    # Function call
+    print(f"gcd({a},{b}) = {gcd(a, b)}")
+    print(f"lcm({a},{b}) = {lcm(a, b)}")
---
+++ gcd_ES_2.py
@@ -0,0 +1,16 @@
+"""Tính ước chung lớn nhất và bội chung nhỏ nhất của 2 số tự nhiên"""
+
+def gcd(a, b):
+    """Tính ước chung lớn nhất của 2 số tự nhiên (theo thuật toán Euclide)"""
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Tính bội chung nhỏ nhất của 2 số tự nhiên"""
+    return int((a * b) / gcd(a,b))
+
+a = int(input('Nhập số tự nhiên a = '))
+b = int(input('Nhập số tự nhiên b = '))
+print(f'Ước chung lớn nhất của {a} và {b} là: {gcd(a,b)}')
+print(f'Bội chung nhỏ nhất của {a} và {b} là: {lcm(a,b)}')
