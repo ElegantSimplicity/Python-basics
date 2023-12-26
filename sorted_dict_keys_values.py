daf6116b1a892d74f6be72ca84e246d71c2a8c5d
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-12-26 13:18:58 +0700

    Add files via upload

---
+++ sorted_dict_keys_values.py
@@ -0,0 +1,23 @@
+# Program 73: sắp xếp từ điển theo key; theo value
+# https://drive.google.com/file/d/1Lf76ElJmwZU3cF7DMrDBun9wTSVtrbfF/view
+# Từ điển (dict) là kiểu dữ liệu gồm các bộ key : value
+# Chú ý: key trong lệnh sorted khác với key trong từ điển
+# (xem https://www.w3schools.com/python/ref_func_sorted.asp)
+
+D = {'Apple': 3, 'Papaya': 1, 'Banana': 4, 'Cherry': 2}
+print("My dict is:      ", D)
+print("Sắp xếp dùng hàm 'sorted'")
+D_keys = dict(sorted(D.items()))
+print("Sorted by keys:  ", D_keys)
+
+D_values = dict(sorted(D.items(), key=lambda item: item[1]))
+print("Sorted by values:", D_values)
+
+###########################################################
+print("\nCách in khác như sau:")
+print("Theo keys", end = " "*10)
+for k, v in D_keys.items():
+    print(f"{k}: {v}", end = " "*5)
+print("\nTheo values", end = " "*8)
+for k, v in D_values.items():
+    print(f"{k}: {v}", end = " "*5)
