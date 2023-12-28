ab58203f51c64e173741c16faf9170a5fe1547f9
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2023-12-28 14:21:18 +0700

    Update Python_keyword_learned.py

--- Python_keyword_learned.py
+++ Python_keyword_learned.py
@@ -1,5 +1,5 @@
 # In ra bảng các từ khóa Python: cả cứng và mềm
-# Tô màu các từ khóa Python đã biết
+# Tô màu các từ khóa plain Python (trước hết nên hiểu thật rõ cách dùng Python mộc)
 # Lưu ý: gõ help('keyword') tại dấu nhắc thì cũng ra bảng
 
 # Hàm in bảng các phần tử của arr, mặc định 5 cột
@@ -22,7 +22,7 @@ def ESprint(arr, column=5):
 
 # Hàm in bảng các phần tử của arr, mặc định 5 cột
 # và các phẩn tử của subarr được in màu đỏ
-def ESprint_with_color(arr, column=5, sub_arr={}):
+def ESprint_with_color(arr, column=5, sub_arr=set()):
     for i in range(len(arr)):
         if i % column == 0: print()
         if arr[i] in sub_arr:
@@ -32,21 +32,22 @@ def ESprint_with_color(arr, column=5, sub_arr={}):
             print(arr[i].ljust(10),end='')
 
 import keyword
-
 kw  = keyword.kwlist        # các từ khóa
 skw = keyword.softkwlist    # các từ khóa mềm
-
 print('All Python keywords'.upper())
-ESprint(kw + skw,6)
+ESprint(kw + skw)
 
 s = '\033[31m' + 'red' + '\033[39m'
-print(f'\n\nAll Python keywords I have learnt in {s}:')
-learned_keywords = {'match','case','_','from','import',
-                    'global','as','and','or','is',
+print(f'\n\nPlain Python keywords are in {s}:')
+plain_keywords = {'match','case','_','import',
+                    'and','or','is',
                     'lambda','return','def',
                     'if','elif','for','else','while',
                     'in','not','True','False','None',
-                    'break','continue','pass'}
-ESprint_with_color(kw + skw,6,learned_keywords)
-t = len(learned_keywords)/ len(kw + skw) * 100
-print(f'\n\nBạn đã học {t:.2f} % từ khóa Python. Keep trying!')
+                    'break','continue','pass','yield'}
+
+ESprint_with_color(kw + skw,5,plain_keywords)
+a = len(plain_keywords)
+b = len(kw + skw)
+c = a/b * 100
+print(f'\n\nBạn đã học {a}/{b} từ khóa Python, khoảng {c:.2f} %. Keep trying!')
