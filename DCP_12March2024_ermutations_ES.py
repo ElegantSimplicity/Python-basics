2c24dd395d58da3a79df842c8ca9f093dc63272b
Le Huy Tien <144594626+ElegantSimplicity@users.noreply.github.com>
2024-03-14 05:36:48 +0700

    Update DCP_12March2024_ermutations_ES.py

--- DCP_12March2024_ermutations_ES.py
+++ DCP_12March2024_ermutations_ES.py
@@ -2,20 +2,20 @@ def permutations(n):
     """List gồm các hoán vị của n phần tử"""
     if n == 1: return [[1]]
     temp = []
-    #for i in range(n):
-    for i in range(n-1,-1,-1):      # Cách viết theo thứ tự khác
-        for p in permutations(n - 1):
+    for i in range(n):
+    #for i in range(n-1,-1,-1):      # Cách viết theo thứ tự khác
+        for p in permutations(n-1):
             temp.append(p[:i] + [n] + p[i:])
     return temp
 
 k = 3
-P = permutations(k)
-print(f'Có {len(P)} hoán vị: {P}')
+Per = permutations(k)
+print(f'Có {len(Per)} hoán vị: {Per}')
 
 #""" Nếu thích viết đẹp hơn
-print(f'Có {len(P)} hoán vị:')
-for p in P:
-    for i in range(len(p)):
-        print(p[i],end='')
+print(f'Có {len(Per)} hoán vị:')
+for per in Per:
+    for k in range(len(per)):
+        print(per[k], end='')
     print(end=' ')
 #"""    
